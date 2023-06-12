55f91a153eb9e0b7fb3acc0588b6ccd0df642018
YOU <joricde@oulook.com>
2023-06-12 20:33:32 +0800

    执行结果模板

---
+++ src/components/ExecutionMS.vue
@@ -0,0 +1,50 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "ExecutionMS",
+  props:['respData'],
+  data(){
+    return {
+      params: {
+        userInfo: '',
+        accountId: '',
+        identification: '',
+        accountType: '',
+        balance: '',
+        time: '',
+        content: '',
+        targetInfo: '',
+        targetId: '',
+        money: '',
+        staffId: '',
+        staffLevel: '',
+      }
+    }
+  },
+  methods:{
+    passData(){
+      this.$emit('passData', this.respData)
+    }
+  }
+})
+</script>
+
+<template>
+  <a-result title="操作结果">
+    <template #extra>
+      <a-typography style="background: var(--color-fill-2); padding: 24px;">
+        <a-typography-paragraph>
+          {{ respData }}
+        </a-typography-paragraph>
+      </a-typography>
+      <a-space>
+        <a-button type="primary">Back</a-button>
+      </a-space>
+    </template>
+  </a-result>
+</template>
+
+<style scoped>
+
+</style>
